cbc1ff180b279b22852f63c11181290cd55e101c
Lasterion <dimastorozhenko2712@gmail.com>
2017-09-27 17:53:29 +0300

    описание
    трехмерная солнечная система на CSS

---
+++ script.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Lasterion on 15.01.2017.
+ */
+// (function(){
+// 	var rotateY = 30,
+// 		rotateX = 80,
+// 		rotate1Y = -30,
+// 		rotate1X = -80;
+// 	document.onkeydown=function(e){
+// 			 if (e.keyCode ===37) {rotateY -=4;;rotate1Y +=4;}
+// 		else if (e.keyCode ===38) {rotateX +=4; rotate1X -=4;}
+// 		else if (e.keyCode ===39) {rotateY +=4; rotate1Y -=4;}
+// 		else if (e.keyCode ===40) {rotateX -=4; rotate1X +=4;}
+
+// 		document.querySelector('.solar-system').style.transform =
+// 		'rotateY(' + rotateY +'deg)'+
+// 		'rotateX(' + rotateX + 'deg)';
+
+// 		document.querySelector('.syka').style.transform =
+// 		'rotateY(' + rotate1Y +'deg)'+
+// 		'rotateX(' + rotate1X + 'deg)';
+// 	}
+// })();
